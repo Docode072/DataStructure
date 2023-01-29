567cf4f55ac1871a14f465bf8fac1e72c3929a9d
Himanshu Singh <120442022+Docode072@users.noreply.github.com>
2023-01-29 11:16:22 +0530

    Create Min operations_GFG.cpp
    Min operations
    EasyAccuracy: 78.5%Submissions: 4K+Points: 2
    Lamp
    You can earn more Geekbits by participating in GFG Weekly Coding Contest  
    
    Given two numbers a and b. In one operation you can pick any non negative integer x and either of a or b. Now if you picked a then replace a with a&x else if you picked b then replace b with b&x.
    
    Return the minimum number of operation required to make a and b equal.
    
    Note: Here & represents bitwise AND operation.
    
    Example 1:
    
    Input:
    a = 5, b = 12
    Output:
    2
    Explanantion:
    In first operation replace 
    a = a&4 = 4
    after that replace 
    b = b&6 = 4
    Hence both are same after applying two
    operations.
    Example 2:
    
    Input: 
    a = 100, b = 100
    Output: 
    0
    Explanation: 
    Already same.
    Your Task:
    You don't need to read, input, or print anything. Your task is to complete the function solve( ), which takes two integers a and b as input parameters and returns the answer.
    
    Expected Time Complexity: O(1)
    Expected Auxiliary Space: O(1)
    Constraints:
    0 ≤ a, b ≤ 109
    
    https://practice.geeksforgeeks.org/problems/5a7e1a52f1b7796238f9efea4c6fda389f26c327/1

---
+++ BITWISE OPERATOR/Min operations_GFG.cpp
@@ -0,0 +1,12 @@
+int solve(int a, int b) {
+        // code here
+        if(a==b){
+            return 0;
+        }
+        if((a&b)== a || (a&b)== b){
+            return 1;
+        }
+    
+         return 2;
+         
+    }
