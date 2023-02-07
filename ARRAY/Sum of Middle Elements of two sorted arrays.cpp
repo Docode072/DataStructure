0fd122ff6d909ec6cd8989e5856817aaf3ed8d12
Himanshu Singh <120442022+Docode072@users.noreply.github.com>
2023-02-07 12:44:01 +0530

    Create Sum of Middle Elements of two sorted arrays.cpp
    Sum of Middle Elements of two sorted arrays
    MediumAccuracy: 47.98%Submissions: 45K+Points: 4
    Lamp
    Stand out from the crowd. Prepare with Complete Interview Preparation  
    
    Given 2 sorted arrays Ar1 and Ar2 of size N each. Merge the given arrays and find the sum of the two middle elements of the merged array.
    
     
    
    Example 1:
    
    Input:
    N = 5
    Ar1[] = {1, 2, 4, 6, 10}
    Ar2[] = {4, 5, 6, 9, 12}
    Output: 11
    Explanation: The merged array looks like
    {1,2,4,4,5,6,6,9,10,12}. Sum of middle
    elements is 11 (5 + 6).
     
    
    Example 2:
    
    Input:
    N = 5
    Ar1[] = {1, 12, 15, 26, 38}
    Ar2[] = {2, 13, 17, 30, 45}
    Output: 32
    Explanation: The merged array looks like
    {1, 2, 12, 13, 15, 17, 26, 30, 38, 45} 
    sum of middle elements is 32 (15 + 17).
     
    
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function findMidSum() which takes  ar1, ar2 and n as input parameters and returns the sum of middle elements. 
    
     
    
    Expected Time Complexity: O(log N)
    Expected Auxiliary Space: O(1)
    
     
    
    Constraints:
    1 <= N <= 103
    1 <= Ar1[i] <= 106
    1 <= Ar2[i] <= 106
    
    https://practice.geeksforgeeks.org/problems/sum-of-middle-elements-of-two-sorted-arrays2305/1?page=2&curated[]=1&sortBy=submissions

---
+++ ARRAY/Sum of Middle Elements of two sorted arrays.cpp
@@ -0,0 +1,17 @@
+int findMidSum(int ar1[], int ar2[], int n) {
+            // code here 
+             vector<int>arr;
+
+            for(int i=0;i<n;i++){
+
+               arr.push_back(ar1[i]);
+
+               arr.push_back(ar2[i]);
+
+            }
+
+            sort(arr.begin(),arr.end());
+
+            return arr[n-1]+arr[n];
+           
+    }
