fb4926823751633700ce57499408373be58b30d4
Himanshu Singh <120442022+Docode072@users.noreply.github.com>
2023-05-14 11:56:17 +0530

    Create Partition Equal Subset Sum_gfg.cpp
    Partition Equal Subset Sum
    MediumAccuracy: 30.24%Submissions: 163K+Points: 4
    Discover your next potential job and a world of opputunities at Job Fair 2023. Start Applying Today! 
    
    Given an array arr[] of size N, check if it can be partitioned into two parts such that the sum of elements in both parts is the same.
    
    Example 1:
    
    Input: N = 4
    arr = {1, 5, 11, 5}
    Output: YES
    Explanation: 
    The two parts are {1, 5, 5} and {11}.
    Example 2:
    
    Input: N = 3
    arr = {1, 3, 5}
    Output: NO
    Explanation: This array can never be 
    partitioned into two such parts.
    
    Your Task:
    You do not need to read input or print anything. Your task is to complete the function equalPartition() which takes the value N and the array as input parameters and returns 1 if the partition is possible. Otherwise, returns 0.
    
    
    Expected Time Complexity: O(N*sum of elements)
    Expected Auxiliary Space: O(N*sum of elements)
    
    
    Constraints:
    1 ≤ N ≤ 100
    1 ≤ arr[i] ≤ 1000
    N*sum of elements ≤ 5*106
    
    https://practice.geeksforgeeks.org/problems/subset-sum-problem2014/1

---
+++ DYNAMIC PROBLEMS/Partition Equal Subset Sum_gfg.cpp
@@ -0,0 +1,31 @@
+ int equalPartition(int n, int arr[])
+    {
+        // code here
+        int sm=0;
+        for(int i = 0;i<n;i++)
+            sm+= arr[i];
+        if(sm%2!=0) return 0;
+        int sum = sm/2;
+        int dp[n+1][sum+1];
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=true;
+        }
+        for(int j=1;j<=sum;j++)
+        {
+            dp[0][j]=false;
+        }
+                for(int i =1; i< n+1;i++){
+            for(int j=1;j < sum+1;j++){
+                if(arr[i-1] <= j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][sum];
+        
+    }
