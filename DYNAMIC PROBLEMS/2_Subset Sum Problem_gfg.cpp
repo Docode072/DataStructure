3a9ebf90d8a71c6ff803a3399c60305d229c8bcb
Himanshu Singh <120442022+Docode072@users.noreply.github.com>
2023-05-14 11:55:15 +0530

    Create 2_Subset Sum Problem_gfg.cpp
    Subset Sum Problem
    MediumAccuracy: 32.0%Submissions: 128K+Points: 4
    Discover your next potential job and a world of opputunities at Job Fair 2023. Start Applying Today! 
    
    Given an array of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum. 
    
    
    Example 1:
    
    Input:
    N = 6
    arr[] = {3, 34, 4, 12, 5, 2}
    sum = 9
    Output: 1 
    Explanation: Here there exists a subset with
    sum = 9, 4+3+2 = 9.
    Example 2:
    
    Input:
    N = 6
    arr[] = {3, 34, 4, 12, 5, 2}
    sum = 30
    Output: 0 
    Explanation: There is no subset with sum 30.
    
    Your Task:  
    You don't need to read input or print anything. Your task is to complete the function isSubsetSum() which takes the array arr[], its size N and an integer sum as input parameters and returns boolean value true if there exists a subset with given sum and false otherwise.
    The driver code itself prints 1, if returned value is true and prints 0 if returned value is false.
     
    
    Expected Time Complexity: O(sum*N)
    Expected Auxiliary Space: O(sum*N)
     
    
    Constraints:
    1 <= N <= 100
    1<= arr[i] <= 100
    1<= sum <= 105
    
    https://practice.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1

---
+++ DYNAMIC PROBLEMS/2_Subset Sum Problem_gfg.cpp
@@ -0,0 +1,25 @@
+ bool isSubsetSum(vector<int>arr, int sum){
+        // code here 
+        int n = arr.size();
+        int dp[n+1][sum+1];
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=true;
+        }
+        for(int j=1;j<=sum;j++)
+        {
+            dp[0][j]=false;
+        }
+        for(int i =1; i< n+1;i++){
+            for(int j=1;j < sum+1;j++){
+                if(arr[i-1] <= j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][sum];
+    }
