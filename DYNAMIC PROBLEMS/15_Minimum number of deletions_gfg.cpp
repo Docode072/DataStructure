a3e7cda9d6679e39902aa6e5f8eb238148b93dda
Himanshu Singh <120442022+Docode072@users.noreply.github.com>
2023-05-16 12:28:19 +0530

    Create 15_Minimum number of deletions_gfg.cpp
    Minimum number of deletions.
    EasyAccuracy: 65.47%Submissions: 16K+Points: 2
    Discover your next potential job and a world of opputunities at Job Fair 2023. Start Applying Today! 
    
    Given a string 'str' of size ‘n’. The task is to remove or delete the minimum number of characters from the string so that the resultant string is a palindrome. Find the minimum numbers of characters we need to remove.
    Note: The order of characters should be maintained.
    
    Example 1:
    
    Input: n = 7, str = "aebcbda"
    Output: 2
    Explanation: We'll remove 'e' and
    'd' and the string become "abcba".
    â€‹Example 2:
    
    Input: n = 3, str = "aba"
    Output: 0
    Explanation: We don't remove any
    character.
    Your Task:  
    You don't need to read input or print anything. Your task is to complete the function minDeletions() which takes the string s and length of s as inputs and returns the answer.
    
    Expected Time Complexity: O(|str|2)
    Expected Auxiliary Space: O(|str|2)
    
    Constraints:
    1 ≤ |str| ≤ 103
    
    https://practice.geeksforgeeks.org/problems/minimum-number-of-deletions4610/1

---
+++ DYNAMIC PROBLEMS/15_Minimum number of deletions_gfg.cpp
@@ -0,0 +1,29 @@
+int minDeletions(string s1, int n) { 
+    //complete the function here 
+        string t = s1;
+        reverse(t.begin() , t.end());
+        string s2 = t;
+    
+    // top-down apporach (tabular method)
+        int m = n;
+       int dp[n+1][m+1];
+        for(int i=0; i<n+1; i++) {
+           for(int j=0; j<m+1; j++) {
+               if(i == 0 || j==0)
+                dp[i][j] = 0;
+            }
+       }
+        
+       for(int i =1;i<n+1;i++)
+         for(int j=1;j<m+1;j++){
+             if(s1[i-1]== s2[j-1]){
+                 dp[i][j] = 1+dp[i-1][j-1];
+             }
+             else{
+                 dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+             }
+         }
+         
+        int lcs = dp[n][m];
+        return n-lcs;
+} 
